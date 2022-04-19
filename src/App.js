589ff9e16bd641b1adfdd13dd80317cc58e560f1
Jacky Chen <tuo17505@temple.edu>
2022-04-18 21:56:15 -0400

    mib

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <div className='App'>
+      <BrowserRouter basename='/'>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
